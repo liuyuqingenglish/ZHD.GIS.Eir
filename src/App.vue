a03135ca3b58e61db3764afd62a293d123841050
liuyuqing <576479931@qq.com>
2020-10-24 18:06:58 +0800

    1.文件添加

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-container>
+    <el-header>标题栏
+    </el-header>
+  <el-container>
+    <el-aside>侧边栏
+      <router-view name="side"></router-view>
+    </el-aside>
+    <el-main>显示栏
+      <router-view name="main">
+
+      </router-view>
+    </el-main>
+  </el-container>
+  </el-container>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    goto: function () {
+      this.$router.push({ name: 'About' })
+    }
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/Sidbar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="sidbar">
+    <h1>This is an about page</h1>
+  </div>
+</template>
